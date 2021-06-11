ebe53f44c60a66606d6484362bc4b76c2bbc8e8a
Kieran Kelleher <kieran.kelleher@libertyinsurance.ie>
2021-06-11 11:49:12 +0100

    commit Creating Branch Master

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args){
-        System.out.println("Halo from Git");
+        System.out.println("Halo from Git Master");
         System.out.println("Commit 2");
         System.out.println("Commit 3");
         System.out.println("Commit 4");
